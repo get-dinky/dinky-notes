b330ba77917f127f57f1e520b8ce22f4296f217c
Julian Prester <hi@julianprester.com>
2024-02-02 11:58:34 +1100

    Add fullscreen panel

--- dinky_notes/__init__.py
+++ dinky_notes/__init__.py
@@ -5,6 +5,7 @@ from PIL import Image, ImageFont, ImageDraw
 import pkg_resources
 from io import BytesIO
 
+from dinky.display_configuration import DisplayConfiguration
 from dinky.layouts.layout_configuration import Zone
 
 hookimpl = pluggy.HookimplMarker("dinky")
@@ -22,19 +23,31 @@ class DinkyNotesPlugin:
         return r.text
 
     @hookimpl
-    def dinky_draw_zone(self, zone: Zone):
+    def dinky_draw(self, zone: Zone, fullscreen: DisplayConfiguration):
+        # Notes
         notes = self._get_notes()
-        im = Image.new('RGB', (zone.width, zone.height), (255, 255, 255))
-        draw = ImageDraw.Draw(im)
 
+        # Fonts
         font_data = pkg_resources.resource_stream('dinky_notes', 'fonts/Roboto-Regular.ttf')
         font_bytes = BytesIO(font_data.read())
         font_header = ImageFont.truetype(font_bytes, 36)
         font_bytes.seek(0)
         font_regular = ImageFont.truetype(font_bytes, 14)
 
-        draw.rectangle((zone.padding, zone.padding, zone.width-zone.padding, zone.padding + 55), fill=self.primary_color)
-        draw.text((zone.padding + 5, zone.padding + 5), "Notes", font=font_header, fill="white")
+        # Panel
+        im_panel = Image.new('RGB', (zone.width, zone.height), (255, 255, 255))
+        draw_panel = ImageDraw.Draw(im_panel)
+        draw_panel.rectangle((zone.padding, zone.padding, zone.width-zone.padding, zone.padding + 55), fill=self.primary_color)
+        draw_panel.text((zone.padding + 5, zone.padding + 5), "Notes", font=font_header, fill="white")
         wrapper = textwrap.TextWrapper(width=int(0.15 * (zone.width - 2 * zone.padding)), drop_whitespace=False, replace_whitespace=False, subsequent_indent='    ')
-        draw.multiline_text((zone.padding + 5, zone.padding + 5 + 55), wrapper.fill(notes), font=font_regular, fill="black")
-        return im
+        draw_panel.multiline_text((zone.padding + 5, zone.padding + 5 + 55), wrapper.fill(notes), font=font_regular, fill="black")
+
+        # Fullscreen
+        im_fullscreen = Image.new('RGB', (fullscreen.width, fullscreen.height), (255, 255, 255))
+        draw_fullscreen = ImageDraw.Draw(im_fullscreen)
+        draw_fullscreen.rectangle((zone.padding, zone.padding, fullscreen.width-zone.padding, zone.padding + 55), fill=self.primary_color)
+        draw_fullscreen.text((zone.padding + 5, zone.padding + 5), "Notes", font=font_header, fill="white")
+        wrapper = textwrap.TextWrapper(width=int(0.18 * (fullscreen.width - 2 * zone.padding)), drop_whitespace=False, replace_whitespace=False, subsequent_indent='    ')
+        draw_fullscreen.multiline_text((zone.padding + 5, zone.padding + 5 + 55), wrapper.fill(notes), font=font_regular, fill="black")
+
+        return im_panel, im_fullscreen
